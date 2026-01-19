44fa6c39dcab71cbe24498d78a54ff6f9d6339d7
Buvalito <ori.roggemans@hotmail.com>
2026-01-19 19:04:53 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'contract-v1';
-const urlsToCache = ['/', '/index.html', '/manifest.json'];
+const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
@@ -7,4 +7,5 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+
 });
