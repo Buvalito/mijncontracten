5268a65040f23cf88e980aaf6c53b23b7b41ab11
Buvalito <ori.roggemans@hotmail.com>
2026-01-31 11:51:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
+  './Contractlybe.svg',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
@@ -44,3 +45,4 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
