c906c55645804faa8851cf3a76b0456eb499b4ff
Buvalito <ori.roggemans@hotmail.com>
2026-01-31 11:59:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'contractly-v2.2';
+const CACHE_NAME = 'contractly-v3';
 
 const urlsToCache = [
   '/',
   '/index.html',
   '/manifest.json',
-  './Contractlybe.svg',
+  '/Contractlybe.svg',
   'https://cdn.tailwindcss.com',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
@@ -45,4 +45,5 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
 
